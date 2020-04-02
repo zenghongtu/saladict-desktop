882a092019beb462794e533fb5dd21d3046460b0
zenghongtu <zenghongtu@gmail.com>
2020-04-02 23:16:07 +0800

    feat: update app iframe style

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,6 @@
 import React, { FunctionComponent, useEffect, useState } from 'react'
 import './App.css'
 
-import '../emulator/dist/core'
-import '../saladict/assets/runtime.9e1a8283.js'
-import '../saladict/assets/18.dcd4ca98.js'
-import '../saladict/assets/1.a3eb564a.js'
-import '../saladict/assets/3.49ec8a94.js'
-import '../saladict/assets/background.a9679961.js'
 import { remote } from 'electron'
 import { AddressInfo } from 'net'
 
@@ -14,8 +8,10 @@ const addressInfo: AddressInfo = remote.getGlobal('addressInfo')
 
 const App: FunctionComponent<{}> = () => {
   return (
-    <div className="App">
+    <div className="app">
       <iframe
+        frameBorder="0"
+        scrolling="no"
         className="iframe"
         src={`http://${addressInfo.address}:${addressInfo.port}/quick-search.html`}
       ></iframe>
