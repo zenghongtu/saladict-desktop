b007e24d5e7f2e927ded11d72042cf03a74c3390
zenghongtu <zenghongtu@gmail.com>
2020-04-04 10:02:31 +0800

    chore: update emulator wp config

--- src/emulator/webpack.config.js
+++ src/emulator/webpack.config.js
@@ -4,25 +4,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: 'cheap-source-map',
+  target: 'electron-renderer',
   context: __dirname,
   entry: {
     core: './lib/core/index.js',
-    background: './lib/background/index.js'
+    background: './lib/background/index.js',
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+  },
+  node: {
+    __dirname: isDev,
+    __filename: isDev,
   },
   plugins: [
     isDev
       ? new HtmlWebpackPlugin({
-        template: 'template.html',
-        inject: false
-      })
-      : null
+          template: 'template.html',
+          inject: false,
+        })
+      : null,
   ].filter(Boolean),
   optimization: {
-    minimize: false
-  }
+    minimize: false,
+  },
 }
