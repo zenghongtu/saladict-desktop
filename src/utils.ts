f73daf362bf98803b391edcc247ef968d7615f64
zenghongtu <zenghongtu@gmail.com>
2020-04-05 18:52:16 +0800

    feat: update store config

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,11 @@
 import Store from 'electron-store'
+import { app, remote } from 'electron'
 
-export const store = new Store()
+export const store = new Store({
+  accessPropertiesByDotNotation: false,
+  watch: true,
+  name: `${(app || remote.app).name}.config`,
+})
 
 export const triggerInputValueChangeEvent = (
   ele: HTMLInputElement,
